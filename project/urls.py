49250a0aedbd498b357186c89521ec1faefc2114
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-23 20:03:10 +0300

    setting image display requirements

--- project/urls.py
+++ project/urls.py
@@ -7,3 +7,5 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
