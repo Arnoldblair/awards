1a656b675cd7ec38da3b9f185f31ce58eb34e08b
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-23 21:11:54 +0300

    creating profile table and its migrations

--- project/models.py
+++ project/models.py
@@ -5,5 +5,12 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User 
 from tinymce.models import HTMLField
 from url_or_relative_url_field.fields import URLOrRelativeURLField
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
+    profile_photo = CloudinaryField('image')
+    bio = HTMLField(max_length=500,default='Your description')
+    website = URLOrRelativeURLField() 
+    phone_number = models.CharField(max_length=10,default='00000')
